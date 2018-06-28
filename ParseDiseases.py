fddcb472256c6e96a698ffbada2d4cd00636e950
evitalis <evitalis@users.noreply.github.com>
2018-06-28 18:36:46 -0500

    Initial commit

---
+++ ParseDiseases.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+
+def get_json(DDPath):
+    """ Return full JSON data for diseases
+
+    Args:
+        DDPath (str): path to the Darkest Dungeon data files
+
+
+    Returns:
+        fulljson (json): entire JSON data from specified file
+    """
+
+    quirklib = "SHARED/QUIRK/quirk_library.json"
+    libpath = os.path.join(DDPath, quirklib)
+    if os.path.exists(libpath):
+        fulljson = {}
+        with open(libpath, "r") as libfile:
+            fulljson = json.loads(libfile.read())
+        return fulljson
+    else:
+        return {}
+
+
+def get_disease_info():
+    dlist = [d for d in get_json().get("quirks", None) if d["is_disease"]]
+    if len(dlist) > 0:
+        dinfo = {}
+        for disease in dlist:
+            dinfo[disease.get("id", None)] = disease.get("buffs", None)
+        return dinfo
+    else:
+        return {}
